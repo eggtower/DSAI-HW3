12ea654f2afcf187125283302b97deab7d91edd8
johnson870930@gmail.com <johnson870930@gmail.com>
2022-05-18 21:44:34 +0800

    feat: main

--- main.py
+++ main.py
@@ -1,3 +1,9 @@
+import pandas as pd
+import numpy as np
+import tensorflow as tf
+from tensorflow import keras
+from sklearn.preprocessing import MinMaxScaler
+import datetime
 
 # You should not modify this part.
 def config():
@@ -20,10 +26,62 @@ def output(path, data):
 
     return
 
+def getData(path1, path2):
+    dateFormatter = "%Y-%m-%d %H:%M:%S"
+    generation = pd.read_csv(path1, encoding='utf-8')
+    consumption = pd.read_csv(path2, encoding='utf-8')
+    
+    h = len(generation)
+    gen = np.array(generation['generation']).reshape(-1,1)
+    con = np.array(consumption['consumption']).reshape(-1,1)
+    last_date = datetime.datetime.strptime(generation["time"][h-1], dateFormatter)
+
+    gScalar = MinMaxScaler()
+    cScalar = MinMaxScaler()
+    
+    gen = gScalar.fit_transform(gen)
+    con = cScalar.fit_transform(con)
+    
+    gen = gen[-72:].reshape(-1,1,72)
+    con = con[-72:].reshape(-1,1,72)
+    
+    return gen[-72:], gScalar, con[-72:], cScalar, last_date
+
+def test_model(gen, con):
+    genModel = tf.keras.models.load_model('generationModel_v1.h5')
+    pdGen = genModel.predict(gen)
+    conModel = tf.keras.models.load_model('consumptionModel_v1.h5')
+    pdCon = conModel.predict(con)
+    return pdGen.reshape(-1, 1), pdCon.reshape(-1, 1)
+
+def rule(pdGen, pdCon, last_date):
+
+    ans = []
+    for i in range(0, len(pdGen)):
+        last_date = last_date + datetime.timedelta(hours=1)
+        gap = pdCon[i] - pdGen[i]
+        if (gap > 0):
+            for j in range(int(gap)):
+                ans.append([str(last_date), "buy", 2.3, 1])
+        elif (gap < 0):
+            for j in range(int(gap)):
+                ans.append([str(last_date), "buy", 2.3, 1])
+            ans.append([last_date, "sell", 1.5, 1])
+    return ans
+
 
 if __name__ == "__main__":
     args = config()
 
-    data = [["2018-01-01 00:00:00", "buy", 2.5, 3],
-            ["2018-01-01 01:00:00", "sell", 3, 5]]
+    gpus = tf.config.experimental.list_physical_devices(device_type='GPU')
+    physical_devices = tf.config.experimental.set_visible_devices(devices=gpus[0], device_type='GPU')
+    
+    gen, gScalar, con, cScalar, last_date = getData(config().generation, config().consumption)
+    pdGen, pdCon = test_model(gen, con)
+    
+    pdGen = gScalar.inverse_transform(pdGen)
+    pdCon = cScalar.inverse_transform(pdCon)
+    
+    data = rule(pdGen, pdCon, last_date)
+    
     output(args.output, data)
